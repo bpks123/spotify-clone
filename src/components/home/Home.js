2fb05d1fd8a5d41b2737303914ddb9263a3102f6
Bipin Kumar <139612124+bpks123@users.noreply.github.com>
2024-01-31 20:00:35 +0530

    updaeed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import {
   Box,
   CardActionArea,
@@ -19,6 +19,29 @@ export default function Home() {
   const [musicList, setMusicList] = useState([])
   const projectId="c91eotf57uop";
 
+ async function Search(){
+      try{
+        const response= await fetch('https://academics.newtonschool.co/api/v1/music/album', {
+          headers: {
+            projectId: projectId,
+          }})
+        const result=await response.json()
+        setMusicList(result.data)
+      }
+      catch(error){
+        console.log(error)
+      }
+      
+  }
+  useEffect(()=>{
+    Search()
+  },[])
+  const displayedMusic = musicList.slice(1, 6);
+  const displayedMusic_two = musicList.slice(6, 11);
+  const displayedMusic_three = musicList.slice(12, 17);
+  const width = window.screen.width;
+
+console.log(musicList)
   return (
     <div className="homePage">
       <div className="homeBody">
@@ -29,18 +52,10 @@ export default function Home() {
           </Link>
         </div>
         <div className="sectionGrid">
-          <div style={{height:"200px",width:"180px",border:"1px solid white",textAlign:"center",padding:"10px"}}>
-            1st music
-          </div>
-          <div style={{height:"200px",width:"180px",border:"1px solid white",textAlign:"center",padding:"10px"}}>
-            2nd music
-          </div>
-          <div style={{height:"200px",width:"180px",border:"1px solid white",textAlign:"center",padding:"10px"}}>
-            3rd music
-          </div>
-          <div style={{height:"200px",width:"180px",border:"1px solid white",textAlign:"center",padding:"10px"}}>
-            4th music
-          </div>
+        {displayedMusic.map((music) => (
+            // <Cards key={music._id} music={music} />
+            <img src={music.image}/>
+          ))}
         </div>
         <div className="sections">
           <span className="sectionHeading">Songs</span>
