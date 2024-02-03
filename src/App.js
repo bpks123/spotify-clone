ec9b9f9a4a69ca39edbf1aac8c78d254c8f81233
Bipin Kumar <139612124+bpks123@users.noreply.github.com>
2024-02-04 00:15:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import {BrowserRouter as Router,Routes,Route,Link,useLocation} from 'react-router-dom'
 import "./styles/App.css";
 import SideBar from './components/sideBar/SideBar';
@@ -12,35 +12,85 @@ import SongCards from './components/Card/SongCards';
 import Login from './components/Login/Login';
 import SignUp from './components/Sinup/Sinup';
 import  {useStateProvider} from "./components/utils/StateProvider"
+import Forget from './components/Login/Forget';
+import ArtistSong from './components/Card/ArtistSong';
+import LikedPage from './components/home/LikedPage';
+
 
 export default function App() {
+  const location = useLocation();
+  const [{ selectedCard, selectedSong, selectedArtist, token }, dispatch] = useStateProvider();
   
-    return (
-      <Router>
-        
-      <div style={{ display: "flex" }}>
+  useEffect(() => {
+    const storedToken = localStorage.getItem("jwtToken");
+
+    if (storedToken) {
+      dispatch({ type: "SET_TOKEN", payload: storedToken });
+    }
+  }, []);
+  
+  return (
+    
+      <>
+      {location.pathname === "/signup" ? (
+        <SignUp />
+      ) : location.pathname === "/login" ? (
+        <Login />
+      ) : location.pathname === "/premium" ? (
+        <Premium />
+      ) : location.pathname === "/forgot" ? (
+        <Forget />
+      ) : (
+        <Main />
+      )}
+      </>
+      
+      
+    );
+}
+const Main=()=>{
+
+  const location = useLocation();
+  const [
+    { selectedCard, selectedSong, selectedArtist, token, favorites },
+    dispatch,
+  ] = useStateProvider();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("jwtToken");
+
+    if (storedToken) {
+      dispatch({ type: "SET_NAME", payload: storedToken });
+    }
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+    if (
+      Array.isArray(storedFavorites) &&
+      storedFavorites.length > 0 &&
+      storedToken
+    ) {
+      dispatch({ type: "ADD_FAVORITE", payload: storedFavorites });
+    }
+  }, []);
+  const width = window.screen.width;
+  return(
+    <div style={{ display: "flex" }}>
         <SideBar/>
         <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-          
-          
           <Navbar/>
             <Routes>
               <Route path='/' element={<Home/>}/>
               <Route path='/search' element={<Search/>}/>
+              <Route path='/album' element={selectedCard ? <AlbumSongs /> : <Home />}/>
+              <Route path='/songs' element={selectedSong ? <SongCards /> : <Home />}/>
+              <Route path="/artist" element={selectedArtist ? <ArtistSong /> : <Home />}/>
+              <Route path="/liked-song" element={favorites ? <LikedPage /> : <Home />}/>
               <Route path='/searchAll' element={<SearchAll/>}/>
-              <Route path='/album' element={<AlbumSongs/>}/>
-              <Route path='/songs' element={<SongCards/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path='/signup' element={<SignUp/>}/>
-              <Route path="/premium" element={<Premium/>}/>
+              
             </Routes>
           
           
         </div>
         
       </div>
-      </Router>
-      
-    );
+  )
 }
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import {BrowserRouter as Router} from 'react-router-dom'
+import reducer,{initialState} from "./components/utils/reducer"
+import { StateProvider } from './components/utils/StateProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Router>
+      <App />
+      </Router>
+    </StateProvider>
+    
   </React.StrictMode>
 );
 
