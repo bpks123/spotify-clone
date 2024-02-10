d937780302faa75819091cd248780f5320ab1ca5
Bipin Kumar <139612124+bpks123@users.noreply.github.com>
2024-02-10 20:13:28 +0530

    completed

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -16,8 +16,9 @@ import SearchIcon from "@mui/icons-material/Search";
 import romantic from "./Romantic.jpg"
 import newrlease from "./newRelease.jpg"
 import hindi from "./bollywood.webp"
-import panjabi from "./punjabi.jpg"
-
+import trending from "./trending.jpg"
+import top20 from "./top-20.png"
+import SongCard from '../Card/SongCard';
 import { useNavigate } from "react-router-dom";
 export default function Search() {
   const navigate = useNavigate();
@@ -25,32 +26,9 @@ export default function Search() {
   const [searchQuery, setSearchQuery] = useState("");
   const [romanticMusic, setRomanticMusic] = useState([]);
   const [musicList, setMusicList] = useState([]);
+  const [allsearchSongs, setAllsearchSongs]=useState([]);
   const [check,setCheck]=useState(true)
-  const projectId="c91eotf57uop";
-
-  async function Search(){
-    try{
-      const response= await fetch('https://academics.newtonschool.co/api/v1/music/album', {
-        headers: {
-          projectId: projectId,
-        }})
-      const result=await response.json()
-      dispatch({ type: "SET_List", payload: result.data })
-
-      setRomanticMusic(result.data)
-      console.log('Romatic')
-      console.log(romanticMusic)
-      console.log(result.data)
-      
-    }
-    catch(error){
-      console.log(error)
-    }
-    
-}
-  useEffect(()=>{
-    Search()
-  },[])
+  const projectId="c91eotf57uop"; 
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
@@ -71,6 +49,9 @@ export default function Search() {
             projectId: projectId,
           }})
           const result=await response.json();
+          console.log('songs')
+          setAllsearchSongs(result.data)
+          console.log(allsearchSongs)
           const songData=result.data[0]
           
           console.log(songData)
@@ -82,16 +63,48 @@ export default function Search() {
 
     }
   }
-  const handleCardClick = () => {
-    if(token){
-      dispatch({ type: "SET_SELECTED_SONG", payload: searchSong });
-    }
-    else{
-      navigate('/login')
-    }
-  };
   function backToSearch(){
     setCheck(!check)
+  }
+  const songSearch= async (input)=>{
+    console.log(input);
+     let url;
+    if(input == "Trending songs"){
+      url = `https://academics.newtonschool.co/api/v1/music/song?filter={"featured":"Trending songs"}`;
+    }
+    else if(input == "Top 50 of this month"){
+      url = `https://academics.newtonschool.co/api/v1/music/song?filter={"featured":"Top 50 of this month"}`;
+    }
+    else if(input == "Top 20 of this week"){
+      url = `https://academics.newtonschool.co/api/v1/music/song?filter={"featured":"Top 20 of this week"}`;
+    }
+    else if(input=="romantic"){
+      url=`https://academics.newtonschool.co/api/v1/music/song?filter={"mood":"romantic"}`;
+    }
+    else if(input=="new"){
+      url=`https://academics.newtonschool.co/api/v1/music/song?sort={"release":1}`
+    }
+    try{
+      const response= await fetch(url, {
+        headers: {
+          projectId: projectId,
+        }})
+      const result=await response.json()
+
+      setMusicList(result.data)
+      console.log('Music List')
+      console.log(musicList)
+      setCheck(!check)
+      
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+  function searchMenu(){
+    setSearchQuery('')
+    dispatch({ type: "SET_SEARCH_SONG", payload: null })
+
   }
   return (
     <>
@@ -110,45 +123,47 @@ export default function Search() {
             />
 
           </div>
-          {searchSong?(<div className="card" style={{ margin: "auto" }}>
-          <Card
-            sx={{ maxWidth: 160, ml: "40px", mt:"10px"}}
-            onClick={() => {
-              handleCardClick();
-            }}>
-            <CardActionArea sx={{ width: "90%" }}>
-              <CardMedia
-                component="img"
-                height="129"
-                width="100"
-                image={searchSong?.thumbnail}
-                alt="green iguana"
-              />
-              <button className="play_btn">
-                <FaPlay className="buttonIcon" />
-              </button>
-              <CardContent sx={{ background: "#000000", color: "#ffffff" }}>
-                <h3 className="title">{searchSong?.title}</h3>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          </div>):(
+          
+          {searchSong?(
+          <div >
+            
+            
+            <button className='backButton'  onClick={()=>searchMenu()}>&larr; Back To Search Menu</button>
+            <div className="homeBody"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            gap: "20px",
+          }}>
+            {allsearchSongs.map((music)=>(
+              <SongCard song={music} key={music._id}/>
+            ))}
+            </div>
+          </div>
+          ):(
             <div className='searchBody'>
-            <div className='item' onClick={()=>backToSearch()}>
+              <div className='item' onClick={()=>songSearch("Trending songs")}>
+              <h1 style={{position:"absolute"}}>Trending</h1>
+              <img src={trending} alt="romanticImg" className='imageFix'/>
+            </div>            
+            <div className='item' onClick={()=>songSearch("romantic")}>
               <h1 style={{position:"absolute"}}>Romantic</h1>
               <img src={romantic} alt="romanticImg" className='imageFix'/>
             </div>
-            <div className='item' onClick={()=>backToSearch()}>
+            <div className='item' onClick={()=>songSearch("new")}>
               <h1 style={{position:"absolute"}}>New Release</h1>
               <img src={newrlease} className='imageFix'/>
             </div>
-            <div className='item' onClick={()=>backToSearch()}>
+            <div className='item' onClick={()=>songSearch("Top 50 of this month")}>
               <h1 style={{position:"absolute"}}>Hindi</h1>
               <img src={hindi} alt="bollywood" className='imageFix'/>
             </div>
-            <div className='item' onClick={()=>backToSearch()}>
-              <h1 style={{position:"absolute"}}>Panjabi</h1>
-              <img src={panjabi} className='imageFix'/>
+            <div className='item' onClick={()=>songSearch("Top 20 of this week")}>
+              <h1 style={{position:"absolute",color:"black"}}>Songs</h1>
+              <img src={top20
+              } className='imageFix'/>
             </div>
             <Footer/>
           </div>   
@@ -175,8 +190,8 @@ export default function Search() {
             alignItems: "center",
             gap: "20px",
           }}>
-            {romanticMusic.map((music) => (
-            <Cards music={music} key={music._id} />
+            {musicList.map((music) => (
+            <SongCard song={music} key={music._id} />
           ))}
             
             </div> 
